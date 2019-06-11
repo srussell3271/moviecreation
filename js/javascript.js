3bce742927cf7d5498d3d82497e95df30867bd13
Shannon Russell <s.russell3271@gmail.com>
2019-06-11 16:29:28 +0000

    finishing

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,4 @@
-/* global $ */
+/*global $*/
 var slideIndex = 1;
 var genres = ["scary","comedy","action"];
 var randomPosters = [];
@@ -7,66 +7,88 @@ var currentUrl = "";
 
 printPosters();
 showSlides(slideIndex);
+$(".slideshow-container").show();
+$(".gallery").hide();
+$(".favoriteGallery").hide();
 
-// click Handlers
+// Click Handlers
 $("#search-button").click(function(){
+    $('.gallery').empty();
+    $(".slideshow-container").hide();
+    $(".gallery").show();
+    $(".favoriteGallery").hide();
+    $(".dots").hide();
+    
     var searchTerm = $('#search-term').val();
     var url ="https://www.omdbapi.com/?apikey=8e9ded79&y=2016&s="+searchTerm;
     
-    $('.movieList').empty();
-    $(".slideshow-container").hide();
-    $(".dots").hide();
-    
     $.ajax({
-       url: url,
-       method: "GET",
-       success: function(response){
-    
+        url: url,
+        method: "GET",
+        success: function(response){
             for (var i = 0; i < response.Search.length; i++) {
                 var poster = response.Search[i].Poster;
-                
+                // check if Url is correct
                 if (poster.startsWith("http")){
                     poster = poster;
                 }
                 else {
                     poster = "https://www.metrorollerdoors.com.au/wp-content/uploads/2018/02/unavailable-image.jpg";
                 }
-                //   appending items
-                $('.movieList').append(
+                //appending items
+                $('.gallery').append(
                   '<div class="container">\
                         <div class="image">\
-                            <img src='+ poster +'>\
+                            <img class="movieList" src='+ poster +' onclick="onClick(this)">\
                         </div>\
                         <div class="content">\
                             <p>Title:'+response.Search[i].Title+'</p>\
                             <p>Year Released:'+response.Search[i].Year+'</p>\
+                            <div class="rating">\
+                               <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>\
+                            </div>\
+                            <h4>Give Your Review On ' +response.Search[i].Title+ '</h4>\
+                            <textarea class="reviewBox">Type your review here</textarea>\
                         </div>\
                     </div>'
                 );
             }
         },
     });
+  
 });
-// Home Button
-$("#home").click(function(){
-    showSlides(slideIndex);
-    printPosters();
-    $('.movieList').empty();
-    $(".slideshow-container").show();
-    $(".dots").show();
-    
+// Favorite Button
+$("#Favorites").click(function(){
+    $('.favoriteGallery').empty();
+    $(".gallery").hide();
+    $(".dots").hide();
+    $(".favoriteGallery").show();
+    for (var i = 0; i < favorites.length; i++) {
+        $('.favoriteGallery').append(
+            '<div class="favoriteImg">\
+                <img class="favImg" src='+ favorites[i] +'>\
+             </div>'
+        );
+    }
 });
-
 // Functions
+function onClick(element) {
+    alert("This Movie Was Added to Your Favorites");
+    currentUrl = element.src;
+    favorites.push(currentUrl);
+}
+function dropDownMenu(idName) {
+   document.getElementById(idName).classList.toggle("show");
+    console.log(idName);
+}
 function printPosters(){
     for ( var i = 0; i < genres.length; i++) {
         retriveRandomPosters(genres[i]);
     }
 }
-
 function retriveRandomPosters(searchTerm){
     var url ="https://www.omdbapi.com/?apikey=8e9ded79&y=2016&s="+searchTerm;
-    var randomNum = 0;
+    var randomNum = 1;
     
     $.ajax({
     url: url,
@@ -75,8 +97,7 @@ function retriveRandomPosters(searchTerm){
         randomNum = Math.floor(Math.random() * response.Search.length);
         var poster = response.Search[randomNum].Poster;
         
-        // Valid Url Check
-        
+        // Valid Url Check        
         if (poster.startsWith("http")){
             poster=poster;
         }
@@ -97,15 +118,12 @@ function retriveRandomPosters(searchTerm){
     },
     });
 }
-
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
-
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
-
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
@@ -124,4 +142,28 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
+}
+function reset(){
+    printPosters();
+    showSlides(slideIndex);
+    $(".slideshow-container").show();
+    $(".gallery").hide();
+    $(".dots").show();
+    $(".favoriteGallery").hide();
+}
+function swapstylesheet(sheet){
+    document.getElementById("pagestyle").setAttribute("href",sheet);
+}
+function favoritedrpdwn(){
+    $('.favoriteGallery').empty();
+    $(".gallery").hide();
+    $(".dots").hide();
+    $(".favoriteGallery").show();
+    for (var i = 0; i < favorites.length; i++) {
+        $('.favoriteGallery').append(
+            '<div class="favoriteImg">\
+                <img class="favImg" src='+ favorites[i] +'>\
+             </div>'
+        );
+    }
 }
